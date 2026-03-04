74731b005fac58dcc68fc89da48f59bc59fa495c
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2026-03-04 16:15:56 +0100

    feat: enhance ProfilePage layout with animated transitions and dynamic padding for improved user experience

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -2,7 +2,6 @@ import 'package:am_i_cooked/components/my_recipes_listview.dart';
 import 'package:am_i_cooked/components/profil_picture_container.dart';
 import 'package:flutter/material.dart';
 
-
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
@@ -17,6 +16,10 @@ class _ProfilePageState extends State<ProfilePage> {
     setState(() {
       _myRecipesExpanded = !_myRecipesExpanded;
     });
+  }
+
+  bool _getMyRecipesExpanded() {
+    return _myRecipesExpanded;
   }
 
   @override
@@ -42,96 +45,83 @@ class _ProfilePageState extends State<ProfilePage> {
         ],
       ),
       backgroundColor: Theme.of(context).colorScheme.surface,
-      body: _myRecipesExpanded
-          ? Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: MyRecipesListview(toggleMyRecipesExpanded: _toggleMyRecipesExpanded,),
-          )
-          : Padding(
-              padding: const EdgeInsets.only(
-                left: 12.0,
-                right: 12.0,
-                bottom: 12.0,
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                spacing: 10.0,
-                children: [
-                  ProfilePictureContainer(
-                    pathImage: "https://i.redd.it/jqop4dqqmdx91.jpg",
-                    isEditIconVisible: true,
-                  ),
-                  Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Padding(
+        padding: EdgeInsets.only(
+          left: _myRecipesExpanded ? 12.0 : 20.0,
+          right: _myRecipesExpanded ? 12.0 : 20.0,
+          bottom: _myRecipesExpanded ? 12.0: 20.0,
+          top: _myRecipesExpanded ? 8.0 : 0,
+        ),
+        child: Column(
+          children: [
+            AnimatedSize(
+              duration: Duration(milliseconds: 200),
+              curve: Curves.easeInOut,
+              child: AnimatedOpacity(
+                duration: Duration(milliseconds: 200),
+                opacity: _myRecipesExpanded ? 0.0 : 1.0,
+                child: _myRecipesExpanded
+                    ? SizedBox.shrink()
+                    : Column(
                         children: [
-                          Text(
-                            "LVL 4",
-                            style: TextStyle(
-                              fontFamily: "bbh_sans_hegarty",
-                              fontSize: 16,
-                              color: Theme.of(
-                                context,
-                              ).colorScheme.onSurfaceVariant,
-                            ),
+                          ProfilePictureContainer(
+                            pathImage: "https://i.redd.it/jqop4dqqmdx91.jpg",
+                            isEditIconVisible: true,
                           ),
-                          Text(
-                            "20%",
-                            style: TextStyle(
-                              fontFamily: "bbh_sans_hegarty",
-                              fontSize: 16,
-                              color: Theme.of(
-                                context,
-                              ).colorScheme.onSurfaceVariant,
-                            ),
+                          SizedBox(height: 10),
+                          Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    "LVL 4",
+                                    style: TextStyle(
+                                      fontFamily: "bbh_sans_hegarty",
+                                      fontSize: 16,
+                                      color: Theme.of(
+                                        context,
+                                      ).colorScheme.onSurfaceVariant,
+                                    ),
+                                  ),
+                                  Text(
+                                    "20%",
+                                    style: TextStyle(
+                                      fontFamily: "bbh_sans_hegarty",
+                                      fontSize: 16,
+                                      color: Theme.of(
+                                        context,
+                                      ).colorScheme.onSurfaceVariant,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              LinearProgressIndicator(
+                                value: 0.2,
+                                minHeight: 8,
+                                color: Theme.of(context).colorScheme.primary,
+                                year2023: false,
+                                backgroundColor: Theme.of(
+                                  context,
+                                ).colorScheme.onSurfaceVariant,
+                              ),
+                            ],
                           ),
+                          SizedBox(height: 10),
                         ],
                       ),
-                      LinearProgressIndicator(
-                        value: 0.2,
-                        minHeight: 8,
-                        color: Theme.of(context).colorScheme.primary,
-                        backgroundColor: Theme.of(
-                          context,
-                        ).colorScheme.onSurfaceVariant,
-                        year2023: false,
-                      ),
-                    ],
-                  ),
-                  Expanded(
-                    child: Container(
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).colorScheme.surface,
-                        borderRadius: BorderRadius.vertical(
-                          top: Radius.circular(30),
-                          bottom: Radius.circular(20),
-                        ),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Theme.of(
-                              context,
-                            ).colorScheme.onSurface.withAlpha(100),
-                            spreadRadius: 2,
-                            blurRadius: 5,
-                            offset: Offset(0, 3), // changes position of shadow
-                          ),
-                        ],
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.only(
-                          left: 15.0,
-                          right: 15.0,
-                          bottom: 5.0,
-                        ),
-                        child: MyRecipesListview(toggleMyRecipesExpanded: _toggleMyRecipesExpanded,),
-                      ),
-                    ),
-                  ),
-                ],
               ),
             ),
+            Expanded(
+              child: MyRecipesListview(
+                toggleMyRecipesExpanded: _toggleMyRecipesExpanded,
+                getMyRecipesExpanded: _getMyRecipesExpanded,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
