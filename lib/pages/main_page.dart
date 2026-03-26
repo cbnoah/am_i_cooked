59d9ca78f2a17b630031ad37d1837a34154c9b8e
cnoah <noah.charrinbourrat@ynov.com>
2026-03-26 23:36:04 +0100

    feat: search page is now accessible when pressing the search bar in the bottom nav section

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -129,6 +129,7 @@ class _MainPageState extends State<MainPage> {
                 ),
                 child: CarouselView.weighted(
                   itemSnapping: true,
+                  enableSplash: false,
                   // WARNING : flexWeights makes the carousel moves very fast if the weights in the beginning are low (flutter bug still not fixed
                   // see https://github.com/flutter/flutter/issues/160350)
                   flexWeights: [2, 7, 2],
