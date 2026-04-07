2fe906cf3ec4f500e693d23be52d51129f872df1
cnoah <noah.charrinbourrat@ynov.com>
2026-04-07 23:06:57 +0200

    feat: initialize authentication session and update home to AuthLayout

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:adaptive_theme/adaptive_theme.dart';
-import 'package:am_i_cooked/pages/main_page.dart';
+import 'package:am_i_cooked/service/auth_service.dart';
 import 'package:am_i_cooked/theme/dark_theme_data.dart';
 import 'package:am_i_cooked/theme/light_theme_data.dart';
+import 'package:am_i_cooked/utils/auth_layout.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final dio = Dio();
+  final authService = AuthService(dio);
+  await authService.bootstrapSession();
+
   runApp(const MyApp());
 }
 
@@ -22,7 +30,7 @@ class MyApp extends StatelessWidget {
         title: 'Am I Cooked?',
         theme: light,
         darkTheme: dark,
-        home: MainPage(),
+        home: AuthLayout(),
       ),
     );
   }
