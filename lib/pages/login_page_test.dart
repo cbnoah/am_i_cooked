f0a7a6f8f04347a400abf893d1d76c682860dd68
cnoah <noah.charrinbourrat@ynov.com>
2026-04-07 23:07:35 +0200

    feat: login test page added

--- lib/pages/login_page_test.dart
+++ lib/pages/login_page_test.dart
@@ -1,3 +1,6 @@
+import 'package:am_i_cooked/pages/register_page_test.dart';
+import 'package:am_i_cooked/service/auth_service.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class LoginPageTest extends StatefulWidget {
@@ -8,11 +11,77 @@ class LoginPageTest extends StatefulWidget {
 }
 
 class _LoginPageTestState extends State<LoginPageTest> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final Dio _dio = Dio();
+  late final AuthService _authService = AuthService(_dio);
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(title: Text("Login Page")),
       body: Center(
-        child: Text("Login Page Test"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          spacing: 5.0,
+          children: [
+            SizedBox(
+              width: 200,
+              child: TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: "Email",
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 200,
+              child: TextField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+            FilledButton(
+              onPressed: () => {
+                if (_emailController.text != "" &&
+                    _passwordController.text != "")
+                  {
+                    _authService.login(
+                      _emailController.text,
+                      _passwordController.text,
+                    ),
+                  }
+                else
+                  {print("missing attribute")},
+              },
+              child: Text("Login"),
+            ),
+            FilledButton(
+              onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const RegisterPageTest(),
+                ),
+              ),
+              child: Text("Register Page"),
+            ),
+          ],
+        ),
       ),
     );
   }
