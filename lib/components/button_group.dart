f418064c3725fe32bc13ce733f0424ffd6bd199a
cnoah <noah.charrinbourrat@ynov.com>
2026-04-14 23:25:13 +0200

    feat: update button navigation to use Go Router for profile page

--- lib/components/button_group.dart
+++ lib/components/button_group.dart
@@ -1,5 +1,5 @@
-import 'package:am_i_cooked/pages/profile_page.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 import '../utils/page_route_builder.dart';
 
@@ -87,7 +87,7 @@ class _ButtonGroupState extends State<ButtonGroup> {
         ),
         FilledButton(
           onPressed: () {
-            Navigator.of(context).push(createRoute(const ProfilePage()));
+            context.push("/profile");
           },
           style: FilledButton.styleFrom(
             minimumSize: const Size(0, buttonHeight),
