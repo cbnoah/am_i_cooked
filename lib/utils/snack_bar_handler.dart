69804d25babe53aa43d526884bc9c80fcd97529d
mathieu <mathieu.beriac@ynov.com>
2026-04-15 10:01:55 +0200

    feat: Added snack bar gestion in utils folder

---
+++ lib/utils/snack_bar_handler.dart
@@ -0,0 +1,21 @@
+  import 'package:flutter/material.dart';
+
+void showErrorSnackbar(String message, BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        backgroundColor: Colors.red,
+        duration: const Duration(seconds: 3),
+      ),
+    );
+  }
+
+  void showSuccessSnackbar(String message, BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        backgroundColor: Colors.green,
+        duration: const Duration(seconds: 2),
+      ),
+    );
+  }
