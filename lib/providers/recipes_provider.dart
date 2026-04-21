97446cc23cf4504f336e7b1022396fe8021be336
cnoah <noah.charrinbourrat@ynov.com>
2026-04-22 00:57:57 +0200

    feat: migrate recipe fetching to Dio and enhance error handling and uniformity across the app

--- lib/providers/recipes_provider.dart
+++ lib/providers/recipes_provider.dart
@@ -2,12 +2,16 @@ import 'dart:convert';
 
 import 'package:am_i_cooked/config/api_config.dart';
 import 'package:am_i_cooked/models/recipe_model.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:http/http.dart' as http;
+
+import '../service/token_service.dart';
 
 final recipesProvider =
-    AsyncNotifierProvider<RecipesNotifier, List<RecipeModel>>(RecipesNotifier.new);
+    AsyncNotifierProvider<RecipesNotifier, List<RecipeModel>>(
+      RecipesNotifier.new,
+    );
 
 final recipeByIdProvider = Provider.family<RecipeModel?, int>((ref, recipeId) {
   final recipesAsync = ref.watch(recipesProvider);
@@ -31,31 +35,42 @@ class RecipesNotifier extends AsyncNotifier<List<RecipeModel>> {
     return _fetchRecipes();
   }
 
+  final Dio _dio = Dio();
+
   Future<List<RecipeModel>> _fetchRecipes() async {
-    final response = await http.get(Uri.parse(ApiConfig.getAllRecipesUrl()));
+    _dio.options.headers['Content-Type'] = 'application/json';
+    _dio.options.headers['access-token'] =
+        'Bearer ${await TokenService.instance.getAccessToken()}';
 
-    if (response.statusCode != 200) {
-      throw Exception('Failed to load recipes data');
-    }
+    try {
+      final response = await _dio.get(ApiConfig.getAllRecipesUrl());
+      if (response.statusCode != 200) {
+        throw Exception('Failed to load recipes data');
+      }
 
-    final decoded = jsonDecode(response.body);
-    if (decoded is! List<dynamic>) {
-      throw Exception('Invalid recipes payload');
-    }
+      if (response.data is! List<dynamic>) {
+        throw Exception('Invalid recipes payload');
+      }
 
-    final recipes = <RecipeModel>[];
+      final recipes = <RecipeModel>[];
 
-    for (final item in decoded) {
-      if (item is! Map<String, dynamic>) continue;
-      try {
-        recipes.add(RecipeModel.fromJson(item));
-      } on FormatException {
-        if (kDebugMode) {
-          print('Error when receiving Recipe data');
+      for (final item in response.data) {
+        if (item is! Map<String, dynamic>) continue;
+        try {
+          recipes.add(RecipeModel.fromJson(item));
+        } on FormatException {
+          if (kDebugMode) {
+            print('Error when receiving Recipe data');
+          }
         }
       }
-    }
 
-    return recipes;
+      return recipes;
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error fetching recipes: $e');
+      }
+      throw Exception('Failed to load recipes data');
+    }
   }
 }
